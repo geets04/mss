f099680a731c5f10791c6ab7a664f9d1140ba92d
cyberorg <cyberorg@opensuse.org>
2017-01-29 11:52:05 +0530

    try fixing guj pdf generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -105,25 +105,18 @@ htmlhelp_basename = 'MysCoolServerdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
-
+latex_engine = 'xelatex'
 latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
+    'inputenc': '',
+    'utf8extra': '',
+    'preamble': '''
 
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
+\usepackage{fontspec}
+\setsansfont{FreeSans}
+\setromanfont{FreeSerif}
+\setmonofont{FreeMono}
+''',
 }
-
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
